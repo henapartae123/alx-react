9fa4db53dd4305cb46ecc3fbd094cf7c7d375349
Henok Shewakena <henyvibe@gmail.com>
2023-01-23 14:28:27 +0300

    task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -5,3 +5,12 @@ $("body").append("<p>Dashboard data for the students</p>");
 $("body").append("button>Click here to get started</button>");
 $("body").append("<p id ='count'></p>");
 $("body").append("<p>Copyright - Holberton School</p>");
+
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
