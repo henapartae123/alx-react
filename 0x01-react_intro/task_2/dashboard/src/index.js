c12f5888423dd315b37bcba5c06133303d7a66ea
Henok Shewakena <henyvibe@gmail.com>
2023-01-25 10:33:26 +0300

    task_2

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Notifications from "./Notifications";
 import reportWebVitals from "./reportWebVitals";
+import Notifications from "./Notifications";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
