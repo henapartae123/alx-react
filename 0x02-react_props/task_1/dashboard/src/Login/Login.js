58ce88e8a4a13e6fb81a47cc3a4e21d93a91dce6
Henok Shewakena <henyvibe@gmail.com>
2023-01-26 11:46:43 +0300

    task_1

---
+++ 0x02-react_props/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,16 @@
+function Login() {
+  return (
+    <React.Fragment>
+      <p>Login to access the full dashboard</p>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email"></input>
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password"></input>
+        <button>OK</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+export default Login;
