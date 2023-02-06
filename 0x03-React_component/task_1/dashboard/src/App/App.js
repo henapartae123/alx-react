8a7c7e08e4108bcfce29b8db9de693c1dea326b3
Henok Shewakena <henyvibe@gmail.com>
2023-02-06 12:00:35 +0300

    task_1

---
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./App.css";
+import Notifications from "../Notifications/Notifications";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import CourseList from "../CourseList/CourseList";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
+
+class App extends React.Component {
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
+
+  render() {
+    return (
+      <React.Fragment>
+        <Notifications listNotifications={listNotifications} />
+        <div className="App">
+          <div className="Header">
+            <Header />
+          </div>
+          <body className="App-body">
+            {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          </body>
+
+          <footer className="App-footer">
+            <Footer />
+          </footer>
+        </div>
+        ;
+      </React.Fragment>
+    );
+  }
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+export default App;
---
+++ 0x03-React_component/task_1/dashboard/src/Notifications/Notitfications.test.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Notifications from "./Notifications";
+
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: getLatestNotification() },
+];
+
+describe("Notification component tests", () => {
+  it("renders Notification component without crashing", () => {
+    const notification = shallow(<Notifications />);
+
+    expect(notification).toBeDefined();
+  });
+
+  it("renders ul", () => {
+    const notification = shallow(<Notifications />);
+
+    expect(notification.find("ul")).toBeDefined();
+  });
+
+  it("renders three list items", () => {
+    const notification = shallow(<Notifications />);
+
+    expect(notification.find("li")).toHaveLength(3);
+  });
+
+  it("renders correct text", () => {
+    const notification = shallow(<Notifications />);
+
+    expect(notification.find("p").text()).toBe(
+      "Here is the list of notifications"
+    );
+  });
+
+  it("does not display menuItem and notifications divs when displayDrawer is false", () => {
+    const wrapper = shallow(<Notifications displayDrawer={false} />);
+
+    expect(wrapper.find("div.menuItem").exists()).toBe(true);
+    expect(wrapper.find("div.Notifications").exists()).toBe(false);
+  });
+
+  it("displays menuItem and Notifications when displayDrawer is true", () => {
+    const wrapper = shallow(<Notifications displayDrawer={true} />);
+
+    expect(wrapper.find("div.menuItem").exists()).toBe(true);
+    expect(wrapper.find("div.Notifications").exists()).toBe(true);
+  });
+
+  it("renders correctly if you pass an empty array", () => {
+    const wrapper = shallow(
+      <Notifications displayDrawer={true} listNotifications={[]} />
+    );
+
+    expect(
+      wrapper.containsMatchingElement(
+        <li data-notification-type="default">No new notificatins</li>
+      )
+    );
+  });
+
+  it("renders correctly when listCourses is not passed", () => {
+    const wrapper = shallow(<Notifications displayDrawer={true} />);
+
+    expect(
+      wrapper.containsMatchingElement(
+        <li data-notification-type="default">No new notification for now</li>
+      )
+    );
+  });
+
+  it('renders "No new notifications for now" instead of "Here is the list of notifications" when listNotifications is empty', () => {
+    const wrapper = shallow(
+      <Notifications displayDrawer={true} listNotifications={[]} />
+    );
+
+    expect(
+      wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)
+    ).toBe(false);
+
+    expect(
+      wrapper.containsMatchingElement(
+        <li data-notification-type="default">No new notification for now</li>
+      )
+    );
+  });
+
+  it('renders "No new notifications for now" instead of "Here is the list of notifications" when listNotifications is empty', () => {
+    const wrapper = shallow(
+      <Notifications displayDrawer={true} listNotifications={[]} />
+    );
+
+    expect(
+      wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)
+    ).toBe(false);
+
+    expect(
+      wrapper.containsMatchingElement(
+        <li data-notification-type="default">No new notification for now</li>
+      )
+    );
+  });
+});
