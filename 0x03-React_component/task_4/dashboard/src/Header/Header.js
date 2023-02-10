91dab940847008667c9adc51f3d9aa56b4cacb88
Henok Shewakena <henyvibe@gmail.com>
2023-02-10 20:58:06 +0300

    HOC

---
+++ 0x03-React_component/task_4/dashboard/src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>School dashboard</h1>
+    </header>
+  );
+}
+
+export default Header;
